e301604358be1a867ccc38989c35c9ace5dbb2ac
Takashi Koike <ukinedori>
2016-07-05 08:23:52 +0900

    add Sagami

--- js/fudoki.js
+++ js/fudoki.js
@@ -6,25 +6,25 @@ $(function(){
   });
 
   // 迅速測図
-  var oldLayer = L.tileLayer(
-    'http://www.finds.jp/ws/tmc/1.0.0/Kanto_Rapid-900913-L/{z}/{x}/{y}.png', {
-       opacity: 0.9,
-       attribution: '<a href="http://www.finds.jp/wsdocs/hawms/index.html" target="_blank">歴史的農業環境WMS配信サービス</a>'
-  });
+//  var oldLayer = L.tileLayer(
+//    'http://www.finds.jp/ws/tmc/1.0.0/Kanto_Rapid-900913-L/{z}/{x}/{y}.png', {
+//       opacity: 0.9,
+//       attribution: '<a href="http://www.finds.jp/wsdocs/hawms/index.html" target="_blank">歴史的農業環境WMS配信サービス</a>'
+//  });
 
   // 地理院地図
   var newLayer = L.tileLayer(
-    'http://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
+    'https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
        opacity: 0.6,
        attribution: '<a href="http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html" target="_blank">国土地理院</a>'
   });
 
   // レイヤコントロール
-  var baseLayers = {
-    "現代": newLayer,
-    "明治期": oldLayer
-  };
-  L.control.layers(baseLayers).addTo(map);
+//  var baseLayers = {
+//    "現代": newLayer,
+//    "明治期": oldLayer
+//  };
+//  L.control.layers(baseLayers).addTo(map);
   newLayer.addTo(map);
 
   L.control.locate({
@@ -86,18 +86,18 @@ $(function(){
           (feature.properties.div1 ? feature.properties.div1 : '') + ' ' +  // 郡
           (feature.properties.div2 ? feature.properties.div2 : '') + ' ' +  // 郷
           (feature.properties.div3 ? feature.properties.div3 : '') + ' ' +  // 庄
-          (feature.properties.div4 ? feature.properties.div4 : '') + ' ' +  // 領
-          (feature.properties.div5 ? 
-            '<div class="remark">' + feature.properties.div5  + '</div>' : '') + ' ' +
-          (feature.properties.pre ? 
-            '<div class="remark">' + feature.properties.pre  + '</div>' : '') + ' ' +
-          feature.properties.village + ' ' +
-          (feature.properties.post ? 
-            '<div class="remark">' + feature.properties.post  + '</div>' : '') + ' ' +
+//          (feature.properties.div4 ? feature.properties.div4 : '') + ' ' +  // 領
+//          (feature.properties.div5 ? 
+//            '<div class="remark">' + feature.properties.div5  + '</div>' : '') + ' ' +
+//          (feature.properties.pre ? 
+//            '<div class="remark">' + feature.properties.pre  + '</div>' : '') + ' ' +
+          feature.properties.name + ' ' +
+//          (feature.properties.post ? 
+//            '<div class="remark">' + feature.properties.post  + '</div>' : '') + ' ' +
           '</strong><hr>' +
           '比定地：' + feature.properties.address + '<br/>' +
-          '風土記稿：<a href="' + feature.properties.source + '" target="_blank">' +
-          feature.properties.source + '</a>'
+          '風土記稿：<a href="' + feature.properties.url + '" target="_blank">' +
+          feature.properties.url + '</a>'
         );
       }
     });
